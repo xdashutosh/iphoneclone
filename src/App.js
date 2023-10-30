4ee337361c95770975be6145de942429e47638bc
ashuchaudhary6969@gmail.com <ashuchaudhary6969@gmail.com>
2023-10-30 14:49:57 +0530

    iphone strc

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
+import { Box, Container, HStack, Stack, Text, VStack } from '@chakra-ui/layout';
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+<Stack  h={'100vh'} w={'full'} justifyContent={'center'} alignItems={'center'}>
+<VStack backgroundImage={'https://wallpapers.com/images/featured/aesthetic-iphone-8k436w42fxqrt0xb.jpg'} bgPosition={'center'} padding={'4'} border={'5px solid'} backgroundSize={'cover'} backgroundRepeat={'no-repeat'} h={'60%'} w={['70%','17%']} borderRadius={'3xl'} p={'.5'}>
+<HStack  h={'3%'} w={'100%'} justifyContent={'space-evenly'} borderTopRadius={'3xl'}   >
+<Text fontSize={'12px'} ml={'-2.5'} mt={'2'} color={'white'} >Airtel</Text>
+<HStack h={'90%'} borderBottomRadius={'xl'} w={'35%'} justifyContent={'center'} bg={'blackAlpha.700'}>
+<Box h={'50%'} w={'40%'}  borderRadius={'2xl'} bg={'blackAlpha.700'}></Box>
+<Box h={'8px'} w={'8px'} borderRadius={'100%'} bg={'blackAlpha.700'}></Box>
+</HStack>
+<HStack mt={'2'}>
+
+</HStack>
+</HStack>
+
+</VStack>
+</Stack>
   );
 }
 
