091f1d8101219c6ea13cda60de8c53f4219e55e5
ashuchaudhary6969@gmail.com <ashuchaudhary6969@gmail.com>
2023-10-30 14:54:41 +0530

    unscrollable

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import React from 'react';
 
 function App() {
   return (
-<Stack  h={'100vh'} w={'full'} justifyContent={'center'} alignItems={'center'}>
-<VStack backgroundImage={'https://wallpapers.com/images/featured/aesthetic-iphone-8k436w42fxqrt0xb.jpg'} bgPosition={'center'} padding={'4'} border={'5px solid'} backgroundSize={'cover'} backgroundRepeat={'no-repeat'} h={'60%'} w={['70%','17%']} borderRadius={'3xl'} p={'.5'}>
+<Stack  h={'100vh'} w={'full'} justifyContent={'center'} alignItems={'center'} overflowY={'hidden'}>
+<VStack backgroundImage={'https://wallpapers.com/images/featured/aesthetic-iphone-8k436w42fxqrt0xb.jpg'} bgPosition={'center'} padding={'4'} mt={['-24','0']} border={'5px solid'} backgroundSize={'cover'} backgroundRepeat={'no-repeat'} h={'60%'} w={['70%','17%']} borderRadius={'3xl'} p={'.5'}>
 <HStack  h={'3%'} w={'100%'} justifyContent={'space-evenly'} borderTopRadius={'3xl'}   >
 <Text fontSize={'12px'} ml={'-2.5'} mt={'2'} color={'white'} >Airtel</Text>
 <HStack h={'90%'} borderBottomRadius={'xl'} w={'35%'} justifyContent={'center'} bg={'blackAlpha.700'}>
